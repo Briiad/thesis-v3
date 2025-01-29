b927005d4cfe7ac25ff502d844fbb8da209b0b3f
Briiad <briiiad.dh@gmail.com>
2025-01-29 11:19:35 +0700

    new

--- data/dataset.py
+++ data/dataset.py
@@ -38,7 +38,7 @@ class CustomVOCDataset(Dataset):
                 A.Resize(height=img_size[0], width=img_size[1]),
                 A.HorizontalFlip(p=flip_prob),
                 A.RandomBrightnessContrast(p=brightness_contrast_prob),
-                A.RandomScale(p=rotate_prob),
+                A.RandomRotate90(p=rotate_prob),
                 A.Normalize(mean=mean, std=std),
                 ToTensorV2()
             ], bbox_params=A.BboxParams(format='pascal_voc', label_fields=['labels']))
