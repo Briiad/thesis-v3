221846d12960d00b83465bc699da09708bc1ef38
Briiad <briiiad.dh@gmail.com>
2025-02-09 15:37:29 +0700

    new

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ from multiprocessing import freeze_support
 
 from model.mobilenetv2_nonbipfn import create_mobilenetv2_ssd
 from model.mobilenetv3_large_nonbipfn import create_mobilenetv3_large_ssd
-from model.proposed_model import create_mobilenetv3_large_fcos
+from model.proposed_model import create_mobilenetv3_fcos
 from data.loader import loaders
 from config.config import train_cfg, data_cfg
 from utils.trainer import Trainer
@@ -10,7 +10,7 @@ from utils.trainer import Trainer
 if __name__ == '__main__':
   freeze_support()
   # Create model and get dataloaders
-  model = create_mobilenetv3_large_fcos(num_classes=data_cfg.num_classes)
+  model = create_mobilenetv3_fcos(num_classes=data_cfg.num_classes)
   train_loader, val_loader, test_loader = loaders()
 
   # Initialize trainer
